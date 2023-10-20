1caa177a664dc8d2a702d35a6a85afe6c27b4e52
Jonathan-Agreda <flavioagreda1990@gmail.com>
2023-10-19 22:50:07 -0500

    Routing

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const os = require('os');
 
 const app = express();
 const port = 3000;
+const ip = os.networkInterfaces().wlo1[0].address;
 
 app.get('/', (req, res) => {
   res.send('Hello World, It´s my Server in Express');
 });
 
-app.listen(port, () => {
-  console.log('Server running on port:',port);
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hello, It´s a new endpoint');
 });
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 500
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on: http://${ip}:${port}`);
+});
+
