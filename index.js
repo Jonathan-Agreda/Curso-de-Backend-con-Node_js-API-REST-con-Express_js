05b40c76f247a4963df4ff2baad1f4a66efd8f09
Jonathan-Agreda <flavioagreda1990@gmail.com>
2023-10-19 22:16:36 -0500

    Express y primer servidor HTTP

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('My App');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World, It´s my Server in Express');
+});
+
+app.listen(port, () => {
+  console.log('Server running on port:',port);
+});
